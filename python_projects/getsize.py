5913a00b84aaff326332f22277835f8e5754e204
uprg <pratikgiri043@gmail.com>
2022-10-25 19:48:32 +0530

    new stuff

--- python_projects/getsize.py
+++ python_projects/getsize.py
@@ -1,5 +1,4 @@
 import os
-#print(os.path.getsize("D:\\phone\\How Database Cursors Help in Fetching Large Result sets from your SQL.mp4"))
 
 for i in os.listdir("D:\\phone"):
     if os.path.getsize(f"D:\\phone\\{i}") == 0:
